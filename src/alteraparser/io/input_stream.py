6891a1da6d61791c1a743f8466e811879d9eb363
Thomas Bollmeier <developer@thomas-bollmeier.de>
2026-01-09 20:03:43 +0100

    Cleanup to start version 2

--- src/alteraparser/io/input_stream.py
+++
@@ -1,7 +0,0 @@
-class InputStream(object):
-
-    def has_next_char(self):
-        raise NotImplemented
-
-    def get_next_char(self):
-        raise NotImplemented
--- src/alteraparser/syntaxgraph/final_vertex.py
+++
@@ -1,7 +0,0 @@
-from .vertex import Vertex, VertexCategory
-
-
-class FinalVertex(Vertex):
-
-    def __init__(self):
-        Vertex.__init__(self, VertexCategory.FINAL)
